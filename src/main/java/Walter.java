a2488154b47da33e1e94a2548ba0cd123cdb787b
Victor <victorauyeungxf@gmail.com>
2026-01-22 18:17:08 +0800

    add exit message

--- src/main/java/Walter.java
+++ src/main/java/Walter.java
@@ -1,16 +1,14 @@
 public class Walter {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-
         String greeting = "____________________________________________________________\n" +
                 " Hello! I'm Walter\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________";
+
+        String exit_msg = " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________";
         System.out.println(greeting);
+
+        System.out.println(exit_msg);
     }
 }

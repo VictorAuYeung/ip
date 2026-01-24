76ebd4b847703196dc08a6091286b092912e9c1b
Victor <victorauyeungxf@gmail.com>
2026-01-24 18:13:41 +0800

    update walter

--- src/main/java/Walter.java
+++ src/main/java/Walter.java
@@ -9,7 +9,6 @@ public class Walter {
         System.out.println(horizontalLine);
 
         Scanner scanner = new Scanner(System.in);
-
         ArrayList<Task> tasks = new ArrayList<>();
 
         while (true) {
@@ -17,73 +16,91 @@ public class Walter {
             System.out.println(horizontalLine);
 
             try {
-                if (input.equals("bye")) {
-                    System.out.println("     Bye. Hope to see you again soon!");
-                    System.out.println(horizontalLine);
-                    break;
-                } else if (input.equals("list")) {
-                    System.out.println("     Here are the tasks in your list:");
-                    for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println("     " + (i + 1) + "." + tasks.get(i));
-                    }
-                } else if (input.startsWith("mark ")) {
-                    int taskIndex = Integer.parseInt(input.substring(5)) - 1;
-                    Task t = tasks.get(taskIndex);
-                    t.markAsDone();
-                    System.out.println("     Nice! I've marked this task as done:");
-                    System.out.println("       " + t);
-                } else if (input.startsWith("unmark ")) {
-                    int taskIndex = Integer.parseInt(input.substring(7)) - 1;
-                    Task t = tasks.get(taskIndex);
-                    t.unmarkAsDone();
-                    System.out.println("     OK, I've marked this task as not done yet:");
-                    System.out.println("       " + t);
-                } else if (input.startsWith("delete ")) {
-                    int taskIndex = Integer.parseInt(input.substring(7)) - 1;
+                String[] inputs = input.split(" ", 2);
+                String commandString = inputs[0].toUpperCase();
 
-                    Task t = tasks.get(taskIndex);
-                    tasks.remove(taskIndex);
+                Command command;
+                try {
+                    command = Command.valueOf(commandString);
+                } catch (IllegalArgumentException e) {
+                    throw new WalterException("I'm sorry, but I don't know what that means :-(");
+                }
 
-                    System.out.println("     Noted. I've removed this task:");
-                    System.out.println("       " + t);
-                    System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
+                switch (command) {
+                    case BYE:
+                        System.out.println("     Bye. Hope to see you again soon!");
+                        System.out.println(horizontalLine);
+                        scanner.close();
+                        return;
 
-                } else {
-                    Task newTask = null;
+                    case LIST:
+                        System.out.println("     Here are the tasks in your list:");
+                        for (int i = 0; i < tasks.size(); i++) {
+                            System.out.println("     " + (i + 1) + "." + tasks.get(i));
+                        }
+                        break;
 
-                    if (input.startsWith("todo")) {
-                        if (input.trim().length() <= 4) {
+                    case MARK:
+                        if (inputs.length < 2) throw new WalterException("Please specify which task to mark.");
+                        int markIndex = Integer.parseInt(inputs[1]) - 1;
+                        Task tMark = tasks.get(markIndex);
+                        tMark.markAsDone();
+                        System.out.println("     Nice! I've marked this task as done:");
+                        System.out.println("       " + tMark);
+                        break;
+
+                    case UNMARK:
+                        if (inputs.length < 2) throw new WalterException("Please specify which task to unmark.");
+                        int unmarkIndex = Integer.parseInt(inputs[1]) - 1;
+                        Task tUnmark = tasks.get(unmarkIndex);
+                        tUnmark.unmarkAsDone();
+                        System.out.println("     OK, I've marked this task as not done yet:");
+                        System.out.println("       " + tUnmark);
+                        break;
+
+                    case DELETE:
+                        if (inputs.length < 2) throw new WalterException("Please specify which task to delete.");
+                        int delIndex = Integer.parseInt(inputs[1]) - 1;
+                        Task tDel = tasks.get(delIndex);
+                        tasks.remove(delIndex);
+                        System.out.println("     Noted. I've removed this task:");
+                        System.out.println("       " + tDel);
+                        System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
+                        break;
+
+                    case TODO:
+                        if (inputs.length < 2 || inputs[1].trim().isEmpty()) {
                             throw new WalterException("The description of a todo cannot be empty.");
                         }
-                        newTask = new Todo(input.substring(5));
+                        Task todo = new Todo(inputs[1]);
+                        tasks.add(todo);
+                        printAdded(todo, tasks.size());
+                        break;
 
-                    } else if (input.startsWith("deadline")) {
-                        if (!input.contains(" /by ")) {
+                    case DEADLINE:
+                        if (inputs.length < 2 || !inputs[1].contains(" /by ")) {
                             throw new WalterException("A deadline must include '/by' to specify the date.");
                         }
-                        String[] parts = input.substring(9).split(" /by ");
-                        if (parts[0].trim().isEmpty()) {
+                        String[] dParts = inputs[1].split(" /by ");
+                        if (dParts[0].trim().isEmpty()) {
                             throw new WalterException("The description of a deadline cannot be empty.");
                         }
-                        newTask = new Deadline(parts[0], parts[1]);
+                        Task deadline = new Deadline(dParts[0], dParts[1]);
+                        tasks.add(deadline);
+                        printAdded(deadline, tasks.size());
+                        break;
 
-                    } else if (input.startsWith("event")) {
-                        if (!input.contains(" /from ") || !input.contains(" /to ")) {
-                            throw new WalterException("An event must include '/from' and '/to' to specify the timing.");
+                    case EVENT:
+                        if (inputs.length < 2 || !inputs[1].contains(" /from ") || !inputs[1].contains(" /to ")) {
+                            throw new WalterException("An event must include '/from' and '/to'.");
                         }
-                        String[] parts = input.substring(6).split(" /from ");
-                        String description = parts[0];
-                        String[] timeParts = parts[1].split(" /to ");
-                        newTask = new Event(description, timeParts[0], timeParts[1]);
-
-                    } else {
-                        throw new WalterException("I'm sorry, but I don't know what that means :-(");
-                    }
-
-                    tasks.add(newTask);
-                    System.out.println("     Got it. I've added this task:");
-                    System.out.println("       " + newTask);
-                    System.out.println("     Now you have " + tasks.size() + " tasks in the list.");
+                        String[] eParts = inputs[1].split(" /from ");
+                        String description = eParts[0];
+                        String[] timeParts = eParts[1].split(" /to ");
+                        Task event = new Event(description, timeParts[0], timeParts[1]);
+                        tasks.add(event);
+                        printAdded(event, tasks.size());
+                        break;
                 }
 
             } catch (WalterException e) {
@@ -96,6 +113,11 @@ public class Walter {
 
             System.out.println(horizontalLine);
         }
-        scanner.close();
+    }
+
+    private static void printAdded(Task task, int size) {
+        System.out.println("     Got it. I've added this task:");
+        System.out.println("       " + task);
+        System.out.println("     Now you have " + size + " tasks in the list.");
     }
 }
